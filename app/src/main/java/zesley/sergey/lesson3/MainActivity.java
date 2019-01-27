aa1e732af527a1fc3ec61470a139808ce980df40
Zes <sergey_the_great@mail.ru>
2019-01-27 15:15:42 +0500

    Добавил AlertDialog.

--- app/src/main/java/zesley/sergey/lesson3/MainActivity.java
+++ app/src/main/java/zesley/sergey/lesson3/MainActivity.java
@@ -1,5 +1,7 @@
 package zesley.sergey.lesson3;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,6 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btnStartConversion;
     private TextView tvConversionStatus;
+    AlertDialog mAlertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnStartConversion = findViewById(R.id.btn_start_conversion);
         tvConversionStatus = findViewById(R.id.textview_conversion_status);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Отменить JPEG->PNG?").setTitle("Идет преобразование")
+                .setPositiveButton("Да", (dialog, which) -> {
+                    btnStartConversion.setEnabled(true);
+
+                });
+        mAlertDialog = builder.create();
+
+
+        btnStartConversion.setOnClickListener(v -> {
+            btnStartConversion.setEnabled(false);
+            mAlertDialog.show();
+        });
+
 
     }
 }
