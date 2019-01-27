ce1df52bbac20e4c6b3152cb633f152530f89c1c
Zes <sergey_the_great@mail.ru>
2019-01-27 14:58:17 +0500

    Подготовили UI.

--- app/src/main/java/zesley/sergey/lesson3/MainActivity.java
+++ app/src/main/java/zesley/sergey/lesson3/MainActivity.java
@@ -2,12 +2,19 @@ package zesley.sergey.lesson3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnStartConversion;
+    private TextView tvConversionStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStartConversion = findViewById(R.id.btn_start_conversion);
+        tvConversionStatus = findViewById(R.id.textview_conversion_status);
+
     }
 }
